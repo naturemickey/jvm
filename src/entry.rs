c5859b44cf1ad66a9d14b88b1ac6e8404c8fd5cc
Michael Chow <mickey_zhouyou@yeah.net>
2019-08-21 21:46:20 +0800

    add some code

--- src/entry.rs
+++ src/entry.rs
@@ -2,10 +2,12 @@ use super::os;
 use super::util::file_util;
 use std::path::Path;
 use std::fs::File;
+use std::option::Option;
+use crate::os::path_list_separator;
 
 pub trait entry {
     fn to_string(&self) -> &str;
-    fn read_class(&self, className: &str) -> (&Vec<u8>, &entry);
+    fn read_class(&self, className: &str) -> Option<(&Vec<u8>, &entry)>;
 }
 
 struct dir_entry<'a> {
@@ -43,11 +45,19 @@ impl dir_entry {
 }
 
 impl zip_entry {
-    fn new(path: &str) -> &zip_entry {}
+    fn new(path: &str) -> &zip_entry {
+        &Self { abs_path: Path::new(path) }
+    }
 }
 
 impl composite_entry {
-    fn new(path: &str) -> composite_entry {}
+    fn new(path: &str) -> composite_entry {
+        let path_list = path.split(path_list_separator);
+        let mut path_vec = Vec::new();
+        for p in path_list {
+
+        }
+    }
 }
 
 impl wildcard_entry {
@@ -58,15 +68,40 @@ impl entry for dir_entry {
     fn to_string(&self) -> &str {
         self.abs_dir.to_string_lossy().as_ref()
     }
-    fn read_class<'a>(&self, className: &str) -> (&Vec<u8>, &'a entry) {
+    fn read_class<'a>(&self, className: &str) -> Option<(&Vec<u8>, &entry)> {
         let path = self.abs_dir.join(className).as_path();
-        let file = file_util::path_to_file(path);
-        (file_util::read_file(file), self)
+        if path.is_file() {
+            let file = file_util::path_to_file(path);
+            Some((file_util::read_file(file), self))
+        } else {
+            None
+        }
     }
 }
 
-impl entry for zip_entry {}
+impl entry for zip_entry {
+    fn to_string(&self) -> &str {
+        self.abs_path.to_string_lossy().as_ref()
+    }
+    fn read_class<'a>(&self, className: &str) -> (&Vec<u8>, &'a entry) {
 
-impl entry for composite_entry {}
+    }
+}
 
-impl entry for wildcard_entry {}
+impl entry for composite_entry {
+    fn to_string(&self) -> &str {
+
+    }
+    fn read_class<'a>(&self, className: &str) -> (&Vec<u8>, &'a entry) {
+
+    }
+}
+
+impl entry for wildcard_entry {
+    fn to_string(&self) -> &str {
+
+    }
+    fn read_class<'a>(&self, className: &str) -> (&Vec<u8>, &'a entry) {
+
+    }
+}
