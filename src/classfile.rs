dac7ce2ecec4e6d4b80bacf663eb63d36f3e9855
Michael Chow <mickey_zhouyou@yeah.net>
2019-09-16 22:31:03 +0800

    complete classReader

--- src/classfile.rs
+++ src/classfile.rs
@@ -15,38 +15,54 @@ impl ClassReader {
     }
 
     pub fn read_u16(&mut self) -> u16 {
-        const s: usize = std::mem::size_of::<u16>();
-        let mut bytes: [u8; s] = [0; s];
-        for i in self.pc..self.pc + s {
+        const S: usize = std::mem::size_of::<u16>();
+        let mut bytes: [u8; S] = [0; S];
+        for i in self.pc..self.pc + S {
             bytes[i - self.pc] = self.data[i];
         }
-        self.pc += s;
+        self.pc += S;
         u16::from_be_bytes(bytes)
     }
 
     pub fn read_u32(&mut self) -> u32 {
-        const s: usize = std::mem::size_of::<u32>();
-        let mut bytes: [u8; s] = [0; s];
-        for i in self.pc..self.pc + s {
+        const S: usize = std::mem::size_of::<u32>();
+        let mut bytes: [u8; S] = [0; S];
+        for i in self.pc..self.pc + S {
             bytes[i - self.pc] = self.data[i];
         }
-        self.pc += s;
+        self.pc += S;
         u32::from_be_bytes(bytes)
     }
 
     pub fn read_u64(&mut self) -> u64 {
-        const s: usize = 8;
-        let mut bytes: [u8; s] = [0; s];
-        for i in self.pc..self.pc + s {
+        const S: usize = 8;
+        let mut bytes: [u8; S] = [0; S];
+        for i in self.pc..self.pc + S {
             bytes[i - self.pc] = self.data[i];
         }
-        self.pc += s;
+        self.pc += S;
         u64::from_be_bytes(bytes)
     }
 
-    pub fn read_u16s(&mut self) -> Vec<u16> {}
+    pub fn read_u16s(&mut self) -> Vec<u16> {
+        let mut n = self.read_u16();
+        let mut v = Vec::new();
+        while n > 0 {
+            v.push(self.read_u16());
+            n -= 1;
+        }
+        v
+    }
 
-    pub fn read_bytes(&mut self, length: usize) -> Vec<i16> {}
+    pub fn read_bytes(&mut self, length: usize) -> Vec<u8> {
+        let mut n = length;
+        let mut v = Vec::new();
+        while n > 0 {
+            v.push(self.read_u8());
+            n -= 1;
+        }
+        v
+    }
 
 //    pub fn read<T>(&mut self) -> T {
 //        const s: usize = std::mem::size_of::<T>();
