62010d7d5c02e06d0d5c0cea47dc91d8af88af17
Michael Chow <mickey_zhouyou@yeah.net>
2019-09-28 19:05:33 +0800

    commit some code

--- src/classfile/ConstantInfo.rs
+++ src/classfile/ConstantInfo.rs
@@ -20,9 +20,21 @@ fn read_constant_info(reader: &mut ClassReader, cp: &ConstantPool) -> Box<dyn Co
 
 fn new_constant_info(tag: u8, reader: &mut ClassReader, cp: &ConstantPool) -> Box<dyn ConstantInfo> {
     let mut c: dyn ConstantInfo = match tag {
+        CONSTANT_Utf8 => ConstantUtf8Info::new(),
         CONSTANT_Integer => ConstantIntegerInfo::new(),
-        // TODO
-        _ => panic!("impossible constant tag."),
+        CONSTANT_Float => ConstantFloatInfo::new(),
+        CONSTANT_Long => ConstantLongInfo::new(),
+        CONSTANT_Double => ConstantDoubleInfo::new(),
+        CONSTANT_Class => ConstantClassInfo::new(cp),
+        CONSTANT_String => ConstantStringInfo::new(cp),
+        CONSTANT_Fieldref => ConstantFieldrefInfo::new(ConstantMemberrefInfo::new(cp)),
+        CONSTANT_Methodref => ConstantMethodrefInfo::new(ConstantMemberrefInfo::new(cp)),
+        CONSTANT_InterfaceMethodref => ConstantInterfaceMethodrefInfo::new(ConstantMemberrefInfo::new(cp)),
+        CONSTANT_NameAndType => ConstantNameAndTypeInfo::new(),
+        CONSTANT_MethodHandle => ConstantMethodHandleInfo::new(),
+        CONSTANT_MethodType => ConstantMethodTypeInfo::new(),
+        CONSTANT_InvokeDynamic => ConstantInvokeDynamicInfo::new(),
+        _ => panic!("java. lang. ClassFormatError: constant pool tag!"),
     };
     c.read_info(reader);
     Box::new(c)
@@ -31,6 +43,14 @@ fn new_constant_info(tag: u8, reader: &mut ClassReader, cp: &ConstantPool) -> Bo
 trait ConstantInfo {
     fn read_info(&mut self, reader: &mut ClassReader) -> Box<dyn ConstantInfo>;
 }
+
+struct ConstantEmptyInfo {}
+impl ConstantInfo for ConstantEmptyInfo{
+    fn read_info(&mut self, reader: &mut ClassReader) -> Box<ConstantInfo> {
+        unimplemented!()
+    }
+}
+const EMPTY_CONSTANT_INFO: Box<ConstantEmptyInfo> = Box::new(ConstantEmptyInfo{});
 
 struct ConstantIntegerInfo {}
 
@@ -43,3 +63,159 @@ impl ConstantInfo for ConstantIntegerInfo {
 }
 
 struct ConstantFloatInfo {}
+
+impl ConstantFloatInfo {
+    fn new() -> ConstantFloatInfo {}
+}
+
+impl ConstantInfo for ConstantFloatInfo {
+    fn read_info(&mut self, reader: &mut ClassReader) -> Box<dyn ConstantInfo> {}
+}
+
+struct ConstantUtf8Info {}
+
+impl ConstantUtf8Info {
+    fn new() -> ConstantUtf8Info {}
+}
+
+impl ConstantInfo for ConstantUtf8Info {
+    fn read_info(&mut self, reader: &mut ClassReader) -> Box<dyn ConstantInfo> {}
+}
+
+struct ConstantLongInfo {}
+
+impl ConstantLongInfo {
+    fn new() -> ConstantLongInfo {}
+}
+
+impl ConstantInfo for ConstantLongInfo {
+    fn read_info(&mut self, reader: &mut ClassReader) -> Box<dyn ConstantInfo> {}
+}
+
+struct ConstantDoubleInfo {}
+
+impl ConstantDoubleInfo {
+    fn new() -> ConstantDoubleInfo {}
+}
+
+impl ConstantInfo for ConstantDoubleInfo {
+    fn read_info(&mut self, reader: &mut ClassReader) -> Box<dyn ConstantInfo> {}
+}
+
+struct ConstantClassInfo {}
+
+impl ConstantClassInfo {
+    fn new(cp: &ConstantPool) -> ConstantClassInfo {}
+}
+
+impl ConstantInfo for ConstantClassInfo {
+    fn read_info(&mut self, reader: &mut ClassReader) -> Box<dyn ConstantInfo> {}
+}
+
+struct ConstantStringInfo {}
+
+impl ConstantStringInfo {
+    fn new(cp: &ConstantPool) -> ConstantStringInfo {}
+}
+
+impl ConstantInfo for ConstantStringInfo {
+    fn read_info(&mut self, reader: &mut ClassReader) -> Box<ConstantInfo> {
+        unimplemented!()
+    }
+}
+
+struct ConstantMemberrefInfo {}
+
+//impl ConstantMemberrefInfo {
+//    fn new(cp: &ConstantPool) -> ConstantMemberrefInfo {}
+//}
+//
+//impl ConstantInfo for ConstantMemberrefInfo {
+//    fn read_info(&mut self, reader: &mut ClassReader) -> Box<ConstantInfo> {
+//        unimplemented!()
+//    }
+//}
+
+struct ConstantFieldrefInfo {}
+
+impl ConstantFieldrefInfo {
+    fn new(info: ConstantMemberrefInfo) -> ConstantFieldrefInfo {}
+}
+
+impl ConstantInfo for ConstantFieldrefInfo {
+    fn read_info(&mut self, reader: &mut ClassReader) -> Box<ConstantInfo> {
+        unimplemented!()
+    }
+}
+
+struct ConstantMethodrefInfo {}
+
+impl ConstantMethodrefInfo {
+    fn new(info: ConstantMemberrefInfo) -> ConstantMethodrefInfo {}
+}
+
+impl ConstantInfo for ConstantMethodrefInfo {
+    fn read_info(&mut self, reader: &mut ClassReader) -> Box<ConstantInfo> {
+        unimplemented!()
+    }
+}
+
+struct ConstantInterfaceMethodrefInfo {}
+
+impl ConstantInterfaceMethodrefInfo {
+    fn new(info: ConstantMemberrefInfo) -> ConstantInterfaceMethodrefInfo {}
+}
+
+impl ConstantInfo for ConstantInterfaceMethodrefInfo {
+    fn read_info(&mut self, reader: &mut ClassReader) -> Box<ConstantInfo> {
+        unimplemented!()
+    }
+}
+
+struct ConstantNameAndTypeInfo {}
+
+impl ConstantNameAndTypeInfo {
+    fn new() -> ConstantNameAndTypeInfo {}
+}
+
+impl ConstantInfo for ConstantNameAndTypeInfo {
+    fn read_info(&mut self, reader: &mut ClassReader) -> Box<ConstantInfo> {
+        unimplemented!()
+    }
+}
+
+struct ConstantMethodHandleInfo {}
+
+impl ConstantMethodHandleInfo {
+    fn new() -> ConstantMethodHandleInfo {}
+}
+
+impl ConstantInfo for ConstantMethodHandleInfo {
+    fn read_info(&mut self, reader: &mut ClassReader) -> Box<ConstantInfo> {
+        unimplemented!()
+    }
+}
+
+struct ConstantMethodTypeInfo {}
+
+impl ConstantMethodTypeInfo {
+    fn new() -> ConstantMethodTypeInfo {}
+}
+
+impl ConstantInfo for ConstantMethodTypeInfo {
+    fn read_info(&mut self, reader: &mut ClassReader) -> Box<ConstantInfo> {
+        unimplemented!()
+    }
+}
+
+struct ConstantInvokeDynamicInfo {}
+
+impl ConstantInvokeDynamicInfo {
+    fn new() -> ConstantInvokeDynamicInfo {}
+}
+
+impl ConstantInfo for ConstantInvokeDynamicInfo {
+    fn read_info(&mut self, reader: &mut ClassReader) -> Box<ConstantInfo> {
+        unimplemented!()
+    }
+}
--- src/classfile/ConstantPool.rs
+++ src/classfile/ConstantPool.rs
@@ -1,21 +1,24 @@
 pub struct ConstantPool {
-    constant_info_s: Box<Vec<dyn ConstantInfo>>
+    constant_info_s: Vec<Box<dyn ConstantInfo>>
 }
 
 impl ConstantPool {
     fn read_constant_pool(reader: &mut ClassReader) -> ConstantPool {
         let mut cp_count = reader.read_u16();
-        let constant_info_s = Boxl::new(Vec::new());
+        let mut constant_info_s = Vec::new();
         let mut cp = ConstantPool { constant_info_s };
 
-        // todo 索引从1开始
-        while --cp_count >= 0 {}
+        // 索引从1开始
+        constant_info_s.push(EMPTY_CONSTANT_INFO);
+        for i in 1..cp_count {
+            // todo
+        }
 
         cp
     }
 
     fn get_constant_info(&self, index: u16) -> &dyn ConstantInfo {
-        &self.constant_info_s[index]
+        self.constant_info_s.get(index as usize).unwrap().as_ref()
     }
 
     fn get_name_and_type(&self, index: u16) -> (&str, &str) {
