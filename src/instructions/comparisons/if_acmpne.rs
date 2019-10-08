6f0beba1ffc1855741e8a7734c3d2699a6053131
Michael Chow <mickey_zhouyou@yeah.net>
2019-10-09 06:49:47 +0800

    commit some code

---
+++ src/instructions/comparisons/if_acmpne.rs
@@ -0,0 +1,18 @@
+struct IF_ACMPNE {
+    base: BranchInstruction
+}
+
+impl Instruction for IF_ACMPNE {
+    fn fetch_operands(&mut self, reader: &mut BytecodeReader) {
+        self.base.fetch_operands(reader);
+    }
+
+    fn execute(&mut self, frame: &mut Frame) {
+        let stack = frame.operand_stack();
+        let val2 = stack.pop_ref();
+        let val1 = stack.stack();
+        if val1 != val2 {
+            self.base.branch(frame);
+        }
+    }
+}
---
+++ src/instructions/comparisons/if_icmplt.rs
@@ -0,0 +1,18 @@
+struct IF_ICMPLT {
+    base: BranchInstruction
+}
+
+impl Instruction for IF_ICMPLT {
+    fn fetch_operands(&mut self, reader: &mut BytecodeReader) {
+        self.base.fetch_operands(reader);
+    }
+
+    fn execute(&mut self, frame: &mut Frame) {
+        let stack = frame.operand_stack();
+        let val2 = stack.pop_int();
+        let val1 = stack.pop_int();
+        if val1 < val2 {
+            self.base.branch(frame);
+        }
+    }
+}
