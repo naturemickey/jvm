cbf71fa77eaee6110b165aa31b406bc7a0bcd839
Michael Chow <mickey_zhouyou@yeah.net>
2019-10-15 22:13:48 +0800

    commit some code

--- src/rtda/heap/method.rs
+++ src/rtda/heap/method.rs
@@ -28,15 +28,51 @@ impl<'a> Method<'a> {
         }
     }
 
-    pub fn is_static(&self) -> bool {
-        unimplemented!()
+    pub fn is_synchronized(&self) -> bool {
+        self.member.access_flags & ACC_SYNCHRONIZED != 0
+    }
+    pub fn is_bridge(&self) -> bool {
+        self.member.access_flags & ACC_BRIDGE != 0
+    }
+    pub fn is_varargs(&self) -> bool {
+        self.member.access_flags & ACC_VARARGS != 0
+    }
+    pub fn is_native(&self) -> bool {
+        self.member.access_flags & ACC_NATIVE != 0
+    }
+    pub fn is_abstract(&self) -> bool {
+        self.member.access_flags & ACC_ABSTRACT != 0
+    }
+    pub fn is_strict(&self) -> bool {
+        self.member.access_flags & ACC_STRICT != 0
     }
 
-    pub fn name(&self) -> &str {
-        unimplemented!()
-    }
 
-    pub fn descriptor(&self) -> &str {
-        unimplemented!()
+    fn is_public(&self) -> bool {
+        self.member.is_public()
+    }
+    fn is_private(&self) -> bool {
+        self.member.is_private()
+    }
+    fn is_protected(&self) -> bool {
+        self.member.is_protected()
+    }
+    fn is_static(&self) -> bool {
+        self.member.is_static()
+    }
+    fn is_synthetic(&self) -> bool {
+        self.member.is_synthetic()
+    }
+    fn name(&self) -> &str {
+        self.member.name()
+    }
+    fn descriptor(&self) -> &str {
+        self.member.descriptor()
+    }
+    fn class(&'a self) -> &'a Class<'a> {
+        self.member.class()
+    }
+    fn is_accessible_to(&'a self, d: &'a Class<'a>) -> bool {
+        self.member.is_accessible_to(d)
     }
 }
